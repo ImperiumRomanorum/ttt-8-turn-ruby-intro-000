7c11f8879678ce76bdf91d5ec34d2069e6a6f89d
Michael Calvelo <agedtoastlet@gmail.com>
2019-09-10 23:29:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,8 @@
 
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  puts board
+end  
 
 def valid_move?(board, index)
   if position_taken(board, index)
